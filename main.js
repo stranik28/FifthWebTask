d7fa3b18cc7bafd2c4e0fc17010a940aaf643d6a
stranik28 <jb357506@gmail.com>
2021-11-17 09:24:14 +0300

    add DOM

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-window.addEventListener("DOMContentLoaded", function (event){
-  document.getElementById("click").addEventListener("click", calculate)  
-});
+window.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById("click").addEventListener("click", calculate)
+})
+
 
 function calculate() {
-    let number = Number(document.getElementById("Guns_Count").value);
-    let price = Number(document.getElementById("Guns_Price").value);
-
-
-    let exp = new RegExp(/^[0-9]+$/i);
-    if (
-        !(
-            exp.test(number) && exp.test(price)
-        )
-    ) {
-        alert("You should input positive integers");
-    } else if (number == "") {
-        alert("You are not input number of guns");
-    } else if (price == "") {
-        alert("You are not input price of guns");
-    } else {
-        const cost = number * price;
+    let product = Number(document.getElementById("Guns_Price").value);
+    let lot = Number(document.getElementById("Guns_Count").value);
+    let regExp = new RegExp(/^[0-9]+$/i);
+    if (product < 0 || lot < 0) {
+        alert("Числа не должны быть отрицательными");
+    }
+    else if (!(regExp.test(product) && regExp.test(lot))) {
+        alert("Нужно писать число!");
+    } else if (product === "") {
+        alert("Укажите цену товара");
+    } else if (lot === "") {
+        alert("Укажите количество единиц товара");
+    }
+    else {
+        const cost = product * lot;
         document.getElementById("cost").innerHTML =
-            "Final price is: " + cost;
+            "Стоимость равна: " + cost + " Руб.";
     }
 }
